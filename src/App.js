37555a63cc5d5b7865081b4cf51d2730d0b3823f
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-01-21 16:44:29 -0300

    update tarefas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
 
-import { useState, useEffect } from 'react';
-import {BsTrash, BsBookMarkCheck, BsBookMarkCheckFill} from 'react-icons/bs';
-
-const API = "http://localhost:5000"
+const API = "http://localhost:5000";
 
 function App() {
-  const [title, setTitle] = useState("")
-  const [time, setTime] = useState("")
-  const [todos, setTodos] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  // Load todo on page load
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => err);
+
+      setLoading(false);
+
+      setTodos(res);
+    };
+
+    loadData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
     const todo = {
       id: Math.random(),
@@ -21,49 +38,94 @@ function App() {
       done: false,
     };
 
-//enviou parar api
-    console.log(todo);
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-    setTitle("")
-    setTime("")
+    setTodos((prevState) => [...prevState, todo]);
+
+    setTitle("");
+    setTime("");
+  };
+  const handleDone = async (todo) => {
+    todo.done = !todo.done;
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) =>
+      prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    );
   };
 
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
-    <div class ="App">
-      <div className='todo-header'>
+    <div className="App">
+      <div className="todo-header">
         <h1>React Todo</h1>
       </div>
-      <div className='form-todo'>
-        <h2>Insira sua proxima tarefa:</h2>
+      <div className="form-todo">
+        <h2>Insira sua próxima tarefa:</h2>
         <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-          <label htmlFor="title">O que voce vai fazer?</label>
-          <input type="text" name='title' placeholder='Titulo da tarefa' 
-          onChange={(e) => setTitle(e.target.value)}
-          value={title || ""}
-          required
-          /> 
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Título da tarefa"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              required
+            />
           </div>
-          <input type='submit' value="Enviar" />
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input
+              type="text"
+              placeholder="Tempo estimado (em horas)"
+              onChange={(e) => setTime(e.target.value)}
+              value={time}
+              required
+            />
+          </div>
+          <input type="submit" value="Criar Tarefa" />
         </form>
       </div>
-      <div className='form-todo'>
-        <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-          <label htmlFor="time">Duracao:</label>
-          <input type="text" name='time' placeholder='Titulo da tarefaTempo estimado (em horas)' 
-          onChange={(e) => setTime(e.target.value)}
-          value={time || ""}
-          required
-          /> 
+      <div className="list-todo">
+        <h2>Lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Duração: {todo.time}h</p>
+            <div className="actions">
+              <span onClick={() => handleDone(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
           </div>
-          <input type='submit' value="Criar tarefa" />
-        </form>
-      </div>
-      <div className='list-todo'>
-        <h2>Lista de Tarefas</h2>
-        {todos.length === 0 && <p>Nao ha tarefas!</p>}
+        ))}
       </div>
     </div>
   );
